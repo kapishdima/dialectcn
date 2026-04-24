6065e1dc278d16997f142e4085b5d852521afd6d
dev_wandry <dima.development@wandry.com.ua>
2026-04-24 16:24:45 +0300

    fix: preview scrolling

--- components/preview/index.tsx
+++ components/preview/index.tsx
@@ -34,7 +34,7 @@ import { UpcomingPayments } from "@/components/preview/cards/upcoming-payments";
 
 export default function Preview02Example() {
   return (
-    <div className="overflow-x-auto overflow-y-hidden bg-muted contain-[paint] [--gap:--spacing(4)] 3xl:[--gap:--spacing(12)] md:[--gap:--spacing(10)] dark:bg-background style-lyra:md:[--gap:--spacing(6)] style-mira:md:[--gap:--spacing(6)]">
+    <div className="min-h-0 flex-1 overflow-auto bg-muted contain-[paint] [--gap:--spacing(4)] 3xl:[--gap:--spacing(12)] md:[--gap:--spacing(10)] dark:bg-background style-lyra:md:[--gap:--spacing(6)] style-mira:md:[--gap:--spacing(6)]">
       <div className="flex w-full min-w-max justify-center">
         <div
           className="grid w-[2400px] grid-cols-7 items-start gap-(--gap) bg-muted p-(--gap) md:w-[3000px] dark:bg-background style-lyra:md:w-[2600px] style-mira:md:w-[2600px] *:[div]:gap-(--gap)"
